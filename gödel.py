4840c42a23f0ce91c9f2679e60a200e94f456339
Ahmad Jarara <ajarara@jarmac.org>
2017-03-17 10:09:25 -0400

    invalidate numbering by using a bytearray

--- gödel.py
+++ gödel.py
@@ -1,20 +1,30 @@
+from math import sqrt
 
 # First we need a list of primes. This is a naive algo, but I'm not
 # really interested in efficiency beyond the first 256 primes as we
 # all know english best-lish
 
-def gen_primes(n, cache=[2,3]):
-    ''' Return a sequence of the first n primes. Utilizes a cache. If
-    I cared enough, I'd use redis, but I don't have enough caffeine to
-    care enough. '''
-    if len(cache) > n:
-        return cache[n - 1:].copy() # so we don't deal with any shenanigans.
+# using a global cache so that we can generate by length and by maximum
+# yes, this is ugly. yes, this probably belongs in a redis store
+# unfortunately I don't have enough caffeine to care about ugly.
+_cache=[2,3]
 
-    while len(cache) < n:
-        cache.append(_next_prime(cache))
+def gen_primes(n):
+    ''' generate the first n primes. return them. '''
+    if len(_cache) > n:
+        return _cache[n - 1:].copy() # so we don't deal with any shenanigans.
 
-    return cache[:n].copy()
+    while len(_cache) < n:
+        _cache.append(_next_prime(_cache))
 
+    return _cache[:n].copy()
+
+
+def gen_primes_until(maximum):
+    while _cache[-1] < maximum:
+        _cache.append(_next_prime(_cache))
+    return _cache.copy()
+        
 
 def _next_prime(prlst):
     ''' given a list of primes, return the next one. '''
@@ -33,4 +43,37 @@ def _next_prime(prlst):
                 break  # break out of the for, no need to continue
             if pr ** 2 > start:  # we found a prime!
                 return start
+    
+
+# now that we can generate a list of primes, we have our bijection: the gödel numbering!
+def gödelize(string):
+    pr = gen_primes(len(string))
+    seq = [pr[index] ** ord(string[index]) for index in range(len(string))]
+    result = 1
+    for num in seq:
+        result = result * num
+    return result
+
+
+# this wouldn't be a bijection if we couldn't formulate the inverse!
+def rev_gödelize(number):
+    ''' infinite loop '''
+    assert number > 0 and (number) == int
+    
+    # cowboy coding best coding
+    exp = []
+    i = 0
+    
+    while number != 1:  # every integer has a prime factorization
+        # avoiding an index error:
+        if i > (len(_cache) -1):
+            pass
+        while number % prime[i] == 0
+            
+        
+        
+
+def bump_cache():
+    _cache.append(_next_prime(_cache))
+        
     
--- tests/test-gödel.py
+++ tests/test-gödel.py
@@ -1,3 +1,4 @@
+# not actual  tests. Not sane tests. Tear downs embedded in them. Sadness abounds! 
 
 def prime_test():
     primes = gen_primes(10)
@@ -7,6 +8,12 @@ def prime_test():
     assert primes[3] == 7
     # it goes up to 11
     assert primes[4] == 11
+
+def _tear_down_prime_test2():
+    _cache = [2, 3]
+    primes = gen_primes_until(28)
+    assert primes[-1] == 29
+    
 
 def _next_prime_test():
     assert _next_prime([2,3]) == 5
