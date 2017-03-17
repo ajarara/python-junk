bb5909d087431cdc720927b22e9c8516f868b731
Ahmad Jarara <ajarara@jarmac.org>
2017-03-17 11:25:56 -0400

    Add operator testing
    Wow this repo is a huge mess.

--- gödel.py
+++ gödel.py
@@ -1,4 +1,5 @@
 from math import sqrt
+import operator
 
 # First we need a list of primes. This is a naive algo, but I'm not
 # really interested in efficiency beyond the first 256 primes as we
@@ -56,24 +57,50 @@ def gödelize(string):
 
 
 # this wouldn't be a bijection if we couldn't formulate the inverse!
-def rev_gödelize(number):
-    ''' infinite loop '''
-    assert number > 0 and (number) == int
+# def rev_gödelize(number):
+#     ''' infinite loop '''
+#     assert number > 0 and (number) == int
     
-    # cowboy coding best coding
-    exp = []
-    i = 0
+#     # cowboy coding best coding
+#     exp = []
+#     i = 0
     
-    while number != 1:  # every integer has a prime factorization
-        # avoiding an index error:
-        if i > (len(_cache) -1):
-            pass
-        while number % prime[i] == 0
+#     while number != 1:  # every integer has a prime factorization
+#         # avoiding an index error:
+#         if i > (len(_cache) -1):
+#             pass
+#         while number % prime[i] == 0
             
-        
-        
-
 def bump_cache():
     _cache.append(_next_prime(_cache))
+
+# -------------------- sane program termination --------------------
+
+def op_btarray(b1, b2, op):
+    return bytes(map(lambda bit1, bit2 : op(bit1, bit2), b1, b2))
+
+# likely possible to extend this to infinitely many args.
+def xor_str(s1, s2, op=operator.xor, en='utf-8'):
+    ''' returns result of mapping op of string in given encoding '''
+    b1 = bytes(s1, en)
+    b2 = bytes(s2, en)
+    return op_btarray(b1, b2, op).decode(en)
+
+# utf-8 defines "A" as 65, "z" as 122. We're gonna try if any of these map to some real letter.
+
+BOTTOM="A"
+TOP="z"
+def valid_ops(op):
+    ''' Op should be symmetric, that is, a ^ b == b ^ a for all a,b, in the domain of ^ '''
+    low = ord(BOTTOM)
+    high = ord(TOP)
+    for i in (range(low, high + 1)):  # include top.
+        j = i  # set j to 0 if the op is not symmetric
+        while j < high + 1:
+            res = op(i, j)
+            if low < res < high:
+                print("{} applied to {}, {} yields valid value: {}\n".format(repr(op), i, j, res))
+            j += 1
+                
         
     
