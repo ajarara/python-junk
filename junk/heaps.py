f5c7762264569ae9310306afaf14b75608b16af8
Ahmad Jarara <ajarara@jarmac.org>
2017-10-04 07:51:32 -0400

    Add minimal test for heapsort, remove class based heap
    Using the implicit representation directly.
    
    I like this approach. The only method I did not define directly was
    insertion (which I'll do now I guess)

--- junk/heaps.py
+++ junk/heaps.py
@@ -1,143 +1,93 @@
-import math
-
 
 def xor_swap(arr, idx1, idx2):
     "Why not?"
-    assert idx1 != idx2
-    arr[idx1] = arr[idx1] ^ arr[idx2]
-    arr[idx2] = arr[idx1] ^ arr[idx2]
-    arr[idx1] = arr[idx1] ^ arr[idx2]
+    if idx1 != idx2:
+        arr[idx1] = arr[idx1] ^ arr[idx2]
+        arr[idx2] = arr[idx1] ^ arr[idx2]
+        arr[idx1] = arr[idx1] ^ arr[idx2]
 
 
-class Heap(object):
-    """
-    This is our inspiration: https://www.youtube.com/watch?v=WCm3TqScBM8
-    We're using the implicit representation. An explicit
-    representation would be using something like an object that stored
-    references to the head and end of the list.. but that is a little
-    awkward even though the abstraction isn't broken. The implicit
-    representation (using an array) is quick to implement and also
-    illustrative in general.
+def swap(arr, idx1, idx2):
+    "Less lines, less fun."
+    temp = arr[idx1]
+    arr[idx1] = arr[idx2]
+    arr[idx2] = temp
 
-    For now this is a max heap... and it only works on integers.
-    The only reason it does is because of xor_swap (which can be
-    modified to work on bytes underlying the object).
-    """
-    def __init__(self, iterable=None):
-        self.heap = []
-        if iterable:
-            for element in iterable:
-                self.insert(element)
 
-    def __repr__(self, opt=False):
-        return repr(self.heap)
+def get_parent_idx(idx):
+    if idx != 0:
+        return (idx - 1) // 2
 
-    def _get_parent_idx(self, idx):
-        # the root has no parent. makes insertion easier to implement.
 
-        # the reason this is a method (even though this function is an
-        # invariant of the heap) is that there is implicit state here,
-        # any passed idx is already in the heap, so this will always
-        # return some other idx that is in the heap
-        if idx != 0:
-            return math.floor((idx - 1)/2)
+def get_left_child_idx(idx):
+    return 2 * idx + 1
 
-    def _get_left_child_idx(self, idx):
-        left = 2 * idx + 1
-        if left < len(self.heap):
-            return left
 
-    def _get_right_child_idx(self, idx):
-        right = 2 * idx + 2
-        if right < len(self.heap):
-            return right
+def get_right_child_idx(idx):
+    return 2 * idx + 2
 
-    def _sift_target(self, idx):
-        """
-        Given an index, decides if the element at that index should
-        swap with a child.  If the index has no child (or the children
-        don't compare) returns None. Otherwise returns the idx to swap.
-        """
-        # this code is a little terse so it warrants explanation I
-        # think. if a node does not have a left child, it cannot have
-        # a right child.  (this also means there is no sift_target and
-        # we return None without any branching.) Further, we delay
-        # getting the right child as late as possible
-        left_idx = self._get_left_child_idx(idx)
-        # we can use python's truthiness here, since child indices are never 0
-        # but we won't, because I tripped up on that when doing heap insertion.
-        if left_idx is not None:
-            # now we need to access the parent and compare it to the left
-            parent = self.heap[idx]
-            left = self.heap[left_idx]
 
-            # in all cases we'll need to know if there is a right
-            # child, so we determine that here
-            right_idx = self._get_right_child_idx(idx)
-            # this is messy. is there any way to make this better?
-            if left > parent:
-                # right > left > parent
-                if right_idx is not None and self.heap[right_idx] > left:
-                    return right_idx
-                else:
-                    # two cases:
-                    # no right child
-                    # right child is less than left.
-                    return left_idx
-            # left > right > parent
-            elif right_idx is not None and self.heap[right_idx] > parent:
+def get_swap_idx(semiheap, idx):
+    # returns None when the node passed no longer needs to be swapped.
+    # hm the question here is should these out of range checks be factored out?
+    left_idx = get_left_child_idx(idx)
+    lh = len(semiheap)
+
+    if left_idx < lh:
+        parent = semiheap[idx]
+        left = semiheap[left_idx]
+
+        # we'll always need this.
+        right_idx = get_right_child_idx(idx)
+        if left > parent:
+            if right_idx < lh and semiheap[right_idx] > left:
                 return right_idx
-
-    def get_max(self):
-        # should I raise an error if getmax is called on an empty heap?
-        # probably. is IndexError the right error to send?
-        # or should I mask it with something else?
-        try:
-            return self.heap[0]
-        except IndexError:
-            raise IndexError("get_max called on an empty heap")
-
-    def insert(self, element):
-        self.heap.append(element)
-        if len(self.heap) > 1:
-            curr_idx = len(self.heap) - 1
-            parent_idx = self._get_parent_idx(curr_idx)
-            while parent_idx is not None and self.heap[parent_idx] < element:
-                xor_swap(self.heap, parent_idx, curr_idx)
-                curr_idx = parent_idx
-                parent_idx = self._get_parent_idx(curr_idx)
-
-    def pop_max(self):
-        if not self.heap:
-            raise IndexError("pop_max called on an empty heap")
-        elif len(self.heap) == 1:
-            return self.heap.pop()
-        else:
-            xor_swap(self.heap, 0, len(self.heap) - 1)
-            out = self.heap.pop()
-            # and now we sift down
-            curr_idx = 0
-            swap_idx = self._sift_target(curr_idx)
-            while swap_idx is not None:
-                xor_swap(self.heap, swap_idx, curr_idx)
-                curr_idx = swap_idx
-                swap_idx = self._sift_target(curr_idx)
-            return out
-
-    def __iter__(self):
-        # iteration should consume the heap?
-        while self.heap:
-            yield self.pop_max()
+            else:
+                return left_idx
+        elif right_idx < lh and semiheap[right_idx] > parent:
+            return right_idx
 
 
-def heapsort(arr, ascending=True):
-    if ascending:
-        return list(reversed([element for element in Heap(arr)]))
-    return [element for element in Heap(arr)]
+# now that this is defined we can define sift_down
+def sift_down(arr, idx):
+    curr_idx = idx
+    swap_idx = get_swap_idx(arr, curr_idx)
+    while swap_idx is not None:
+        xor_swap(arr, curr_idx, swap_idx)
+        curr_idx = swap_idx
+        swap_idx = get_swap_idx(arr, curr_idx)
 
 
-# to define heapify in place...
-# hm I'm going to need all of the above operations to operate on some
-# arbitrary reference. Good time to commit here, mangle the object
-# into a bunch of functions (all operating on an array) and define
-# heapify with these 'free' operations
+def heapify(arr):
+    # the last half we initially preserve, excluding the middle (if it exists)
+    idx = len(arr) // 2
+
+    # resist the temptation to use some array slicing mechanism, as
+    # that creates a copy and this is an in place sort :)
+    # we want to decrement until -1
+    while idx >= 0:
+        sift_down(arr, idx)
+        idx -= 1
+
+
+def pop_max(heap):
+    if heap == []:
+        raise IndexError("pop_max called on an empty heap")
+    elif len(heap) == 1:
+        return heap.pop()
+    else:
+        xor_swap(heap, 0, len(heap) - 1)
+        out = heap.pop()
+
+        # now bubble down the inaccurate value at the top
+        sift_down(heap, 0)
+        # heap is balanced
+        return out
+
+
+def heapsort(arr):
+    heapify(arr)
+    out = []
+    for _ in range(len(arr)):
+        out.append(pop_max(arr))
+    arr.extend(out)
--- tests/test-heaps.py
+++ tests/test-heaps.py
@@ -1,5 +1,9 @@
-from junk.heaps import Heap
+from junk.heaps import heapsort
+from random import shuffle
 
 
-def test_working():
-    assert True
+def test_heapsort():
+    ordered = list(range(500))
+    shuffled = ordered.copy()
+    shuffle(shuffled)
+    assert [out for out in heapsort(shuffled, ascending=True)] == ordered
